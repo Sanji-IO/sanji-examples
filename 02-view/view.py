35746e8eb85387c16283f343a6128bb3d7e05bad
Zack YL Shih <zackyl.shih@moxa.com>
2014-09-26 10:47:12 +0800

    Add 02-view

---
+++ 02-view/view.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import logging
+from time import sleep
+
+from sanji.core import Sanji
+from sanji.connection.mqtt import Mqtt
+
+
+class View(Sanji):
+
+    # This function will be executed after registered.
+    def run(self):
+
+        for count in xrange(0, 100, 1):
+            # Normal CRUD Operation
+            #   self.publish.[get, put, delete, post](...)
+            # One-to-One Messaging
+            #   self.publish.direct.[get, put, delete, post](...)
+            #   (if block=True return Message, else return mqtt mid number)
+            # Agruments
+            #   (resource[, data=None, block=True, timeout=60])
+
+            res = self.publish.get("/hellosanji")
+            print "GET /hellosanji"
+            print res.to_json()
+            sleep(3)
+            print "PUT /hellosanji"
+            res = self.publish.put("/hellosanji",
+                                   data={"message": "Hello Sanji! %s" % count})
+            print res.to_json()
+
+
+if __name__ == '__main__':
+    FORMAT = '%(asctime)s - %(levelname)s - %(lineno)s - %(message)s'
+    logging.basicConfig(level=0, format=FORMAT)
+    logger = logging.getLogger('Hellosanji')
+
+    view = View(connection=Mqtt())
+    view.start()
